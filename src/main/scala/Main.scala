7537611975aedf6d4887d98ccf6d7e9ccbbcd230
Manish Mishra <manish.mishra@knoldus.com>
2023-03-06 22:52:31 +0530

    Added String Palindrome File

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,36 @@
+import scala.io.StdIn.readLine
+class StringPalindrome{
+    //To check the string is palindrome or not and return the value
+    def isPalindrome(inputString:String): Boolean= {
+        var lastIndex= inputString.length - 1
+        for (index <- 0 to inputString.length - 1) {
+            if (inputString.charAt(index) != inputString.charAt(lastIndex)) {
+                return false
+            }
+            lastIndex-=1
+        }
+        return true
+    }
+
+    //Method to print the string is palindrome or not
+    def printResult(result: Boolean): Unit = {
+        if (result==false)
+            print("No, It is not Palindrome")
+        else
+            print("Yes, It is Palindrome")
+    }
+}
+//singleton object
+object Palindrome extends App{
+    val obj=new StringPalindrome
+    try{
+        val inputString=readLine("Enter the String  :")
+        val result = obj.isPalindrome(inputString)
+        obj.printResult(result)
+    }
+    catch{
+        case input: StringIndexOutOfBoundsException =>{
+            print("String Overflow")
+        }
+    }
+}
