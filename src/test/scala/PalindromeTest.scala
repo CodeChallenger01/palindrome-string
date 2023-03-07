97ea923569d37465137a9eb6feb27f3811480b3b
Manish Mishra <manish.mishra@knoldus.com>
2023-03-07 11:34:39 +0530

    Updated Test case

--- src/test/scala/PalindromeTest.scala
+++ src/test/scala/PalindromeTest.scala
@@ -2,8 +2,12 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class PalindromeTest extends AnyFlatSpec{
   val obj=new StringPalindrome
-  //Test case fail
+  //Test case pass if we use false
   it should "match" in{
-    assert("Manish"==obj.isPalindrome("Manish"))
+    assert(false==obj.isPalindrome("Manish"))
+  }
+  //Test case pass if we use true
+  it should "match1" in {
+    assert(true== obj.isPalindrome("aba"))
   }
 }
